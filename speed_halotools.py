b2aacf48a5fc11ebf5d3f9e16ed794fd0c7d4008
Leandro Martinez <leandro@iqm.unicamp.br>
2021-06-16 17:36:02 -0300

    test data

--- speed_halotools.py
+++ speed_halotools.py
@@ -1,15 +1,20 @@
 import numpy as np
 import time
+import sys
 from halotools.mock_observables import mean_radial_velocity_vs_r
 
-boxsize = 250.
+if len(sys.argv) == 1 :
+    n_halos = 100_000
+else :
+    n_halos = int(sys.argv[1])
 
-#density = 10**4 / 250**3
-#boxsize = (n_halos / density)**(1/3)
+if len(sys.argv) == 3 :
+    density = 10**5 / 250**3
+    boxsize = (n_halos / density)**(1/3)
+else : 
+    boxsize = 250.
 
-n_halos = 100_000
-
-n_threads = 1
+n_threads = 8
 rbins = [0.,2.,4.,6.,8.,10.]
 Lbox = [boxsize,boxsize, boxsize]
 
@@ -27,7 +32,7 @@ v_12_halotools = mean_radial_velocity_vs_r(
     velocities, 
     rbins_absolute=rbins,
     period=Lbox,
-    num_threads=1,
+    num_threads=8,
 )
 t_halotools = time.time()
 print("Mean radial velocity = ", v_12_halotools)
