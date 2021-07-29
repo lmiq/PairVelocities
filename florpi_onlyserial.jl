709fdb04b53be4ac9b5d6edb08130c4e87704eae
Leandro Martinez <leandro@iqm.unicamp.br>
2021-07-29 08:36:08 -0300

    up

--- florpi_onlyserial.jl
+++ florpi_onlyserial.jl
@@ -4,12 +4,8 @@ using Plots, Plots.Measures
 using DelimitedFiles
 using LaTeXStrings
 
-function run_benchmark(output=false)
+function run_benchmark()
 
-  if Threads.nthreads() != 8 
-    error(" Run with julia -t auto ")
-  end
-  
   ns = [  10000   
           50000   
           100000  
@@ -60,64 +56,33 @@ function run_benchmark(output=false)
   data_cv = readdlm(previous_cv,comments=true,comment_char='#')
   println(" Previous: $previous_cv ")
   
-  new_cv = zeros(length(ns),5)
-  new_cv[1:size(data_cv,1),1:3] .= data_cv[:,1:3]
-
-  new_cd = zeros(length(ns),5)
-  new_cd[1:size(data_cd,1),1:3] .= data_cd[:,1:3]
-
-  #
-  # Parallel 
-  #
-  println("Parallel, constant volume:")
-  CellListMap.florpi(N=1000,cd=false,parallel=true);
-  for i in 1:length(ns)
-    n = ns[i]
-    prev = try data_cv[i,5] catch; 0 end
-    t = @elapsed CellListMap.florpi(N=n,cd=false,parallel=true);
-    new_cv[i,5] = t
-    println(n," ",t," prev ", prev)
-  end
-  
-  println("Parallel, constant density:")
-  CellListMap.florpi(N=1000,cd=true,parallel=true);
-  for i in 1:length(ns)
-    n = ns[i]
-    prev = try data_cd[i,5] catch; 0 end
-    t = @elapsed CellListMap.florpi(N=n,cd=true,parallel=true);
-    new_cd[i,5] = t
-    println(n," ",t," prev ", prev)
-  end
-  
   #
   # Serial
   #
   println("Serial, constant density:")
   CellListMap.florpi(N=1000,cd=true,parallel=false);
+  times1 = Float64[]
   for i in 1:length(ns)
     n = ns[i]
-    prev = try data_cd[i,4] catch; 0 end
+    prev = data_cd[i,4]
     t = @elapsed CellListMap.florpi(N=n,cd=true,parallel=false);
-    new_cd[i,4] = t
+    push!(times1,t)
     println(n," ",t," prev ", prev)
   end
   
   println("Serial, constant volume:")
   CellListMap.florpi(N=1000,cd=false,parallel=false);
-  for i in 1:length(ns)
+  times3 = Float64[]
+  for i in 1:15
     n = ns[i]
-    prev = try data_cv[i,4] catch; 0 end
+    prev = data_cv[i,4]
     t = @elapsed CellListMap.florpi(N=n,cd=false,parallel=false);
-    new_cv[i,4] = t
+    push!(times3,t)
     println(n," ",t," prev ", prev)
   end
   
-  if output
-    writedlm("./data/cd_v$version.dat",new_cd)
-    writedlm("./data/cd_v$version.dat",new_cv)
-    plot_florpi($version)
-  end
-
+  return nothing
+  
 end
 
 
